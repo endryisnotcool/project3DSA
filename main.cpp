35f3aae0d3069a6df8d85752df95675b28b3bf0f
jackhernandez23 <jack.hernandez@ufl.edu>
2024-04-18 16:01:04 -0400

    pushing

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ void setText(Text &text, float x, float y){
     text.setOrigin(textRect.left + textRect.width/2.0f, textRect.top + textRect.height/2.0f);
     text.setPosition(sf::Vector2f(x,y));
 }
+
 int main() {
     RenderWindow welcomeWindow(VideoMode(800, 800), "songSearcher");
     Font font;

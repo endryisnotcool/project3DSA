fa477bae4e67abd18d32908b80e42da1ac52aa58
DanielleSamaroo <127218540+DanielleSamaroo@users.noreply.github.com>
2024-04-19 04:03:07 -0400

    i just changed some things to const

--- song.h
+++ song.h
@@ -8,7 +8,6 @@
 
 using namespace std;
 
-
 class song {
 private:
     const vector<string> ARTIST;
@@ -23,43 +22,43 @@ public:
 
     //constructor
     song(vector<string> artist, string title, string album, bool isExplicit, int popularity, string trackID, string genre)
-    : ARTIST(artist), TITLE(title), ALBUM(album), EXPLICIT(isExplicit), POPULARITY(popularity), TRACKID(trackID), GENRE(genre) {}
+            : ARTIST(artist), TITLE(title), ALBUM(album), EXPLICIT(isExplicit), POPULARITY(popularity), TRACKID(trackID), GENRE(genre) {}
 
     //getter functions
-    vector<string> getArtist();
-    string getTitle();
-    string getAlbum();
-    bool isExplicit();
-    int getPopularity();
-    string getTrackID();
-    string getGenre();
+    vector<string> getArtist() const;
+    string getTitle() const;
+    string getAlbum() const;
+    bool isExplicit() const;
+    int getPopularity() const;
+    string getTrackID() const;
+    string getGenre() const;
 };
 
-vector<string> song::getArtist() {
+vector<string> song::getArtist() const {
     return ARTIST;
 }
 
-string song::getTitle() {
+string song::getTitle() const {
     return TITLE;
 }
 
-string song::getAlbum() {
+string song::getAlbum() const {
     return ALBUM;
 }
 
-bool song::isExplicit() {
+bool song::isExplicit() const {
     return EXPLICIT;
 }
 
-int song::getPopularity() {
+int song::getPopularity() const {
     return POPULARITY;
 }
 
-string song::getTrackID() {
+string song::getTrackID() const {
     return TRACKID;
 }
 
-string song::getGenre(){
+string song::getGenre() const {
     return GENRE;
 }
 

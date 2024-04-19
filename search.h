8080b866d0243f95d37942c06e87c7aff4bd3e89
DanielleSamaroo <127218540+DanielleSamaroo@users.noreply.github.com>
2024-04-19 04:07:35 -0400

    Update search.h

--- search.h
+++ search.h
@@ -6,7 +6,8 @@
 #include <vector>
 #include <unordered_map>
 #include "redblacktree.h"
-#include "song.h"
+
+string toLowerCase(string s);
 
 void search(const std::string& query, const std::unordered_map<std::string, std::vector<song>>& artistMap,
             const std::unordered_map<std::string, std::vector<song>>& genreMap, const RBTree& songTree,
